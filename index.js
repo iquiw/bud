552aea53e13a12f2ae32fefc504e669c3e1a62ab
Iku Iwasa <iku.iwasa@gmail.com>
2017-08-04 08:39:51 +0900

    Move sample data from daychart.js to index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import moment from 'moment';
+
+import { Burndown, Issue } from './lib/burndown';
 import DayChart from './lib/daychart';
 
+let issues = [
+  Issue.opened(moment([2017, 7, 10])),
+  Issue.opened(moment([2017, 7, 12])),
+  Issue.closed(moment([2017, 7, 14]), moment([2017, 7, 14])),
+  Issue.closed(moment([2017, 7, 11]), moment([2017, 7, 14]))
+];
+
+let burndown = new Burndown(issues);
+
 let dc = new DayChart(document.getElementById('myChart').getContext('2d'));
-dc.draw(moment([2017, 7, 10]), moment([2017, 7, 18]));
+dc.draw(moment([2017, 7, 10]), moment([2017, 7, 18]), burndown);
