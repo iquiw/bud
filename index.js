0f629a5955e7b30960100c18065fe11e3a2af181
Iku Iwasa <iku.iwasa@gmail.com>
2017-11-02 23:52:32 +0900

    Wrap moment with day function

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ import moment from 'moment';
 import { Burndown, Issue } from './lib/burndown';
 import DayChart from './lib/daychart';
 
+function day(y, m, d) {
+  return moment([y, m - 1, d]);
+}
+
 let issues = [
-  Issue.opened(moment([2017, 7, 10])),
-  Issue.opened(moment([2017, 7, 12])),
-  Issue.closed(moment([2017, 7, 14]), moment([2017, 7, 14])),
-  Issue.closed(moment([2017, 7, 11]), moment([2017, 7, 14]))
+  Issue.opened(day(2017, 8, 10)),
+  Issue.closed(day(2017, 8, 11), day(2017, 8, 11)),
+  Issue.closed(day(2017, 8, 11), day(2017, 8, 14)),
+  Issue.closed(day(2017, 8, 11), day(2017, 8, 16)),
+  Issue.opened(day(2017, 8, 12)),
+  Issue.closed(day(2017, 8, 12), day(2017, 8, 15)),
+  Issue.closed(day(2017, 8, 14), day(2017, 8, 14)),
+  Issue.closed(day(2017, 8, 15), day(2017, 8, 15))
 ];
 
 let burndown = new Burndown(issues);
 
 let dc = new DayChart(document.getElementById('myChart').getContext('2d'));
-dc.draw(moment([2017, 7, 10]), moment([2017, 7, 18]), burndown);
+dc.draw(day(2017, 8, 12), day(2017, 8, 18), burndown);
