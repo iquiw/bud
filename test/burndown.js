469c0ae583fefeeb838876ac2b15e81626d11b9d
Iku Iwasa <iku.iwasa@gmail.com>
2017-11-09 08:48:21 +0900

    Test all open and all closed cases

--- test/burndown.js
+++ test/burndown.js
@@ -23,3 +23,37 @@ test('Burndown', t => {
   t.is(velocityTotal, 0);
   t.is(velocityClose, - 2 / 9);
 });
+
+test('All open', t => {
+  let issues = [
+    Issue.opened(day(2017, 10, 29)),
+    Issue.opened(day(2017, 10, 30)),
+    Issue.opened(day(2017, 11,  1)),
+    Issue.opened(day(2017, 11,  2)),
+  ];
+  let burndown = new Burndown(issues);
+  let { downs, ups, velocityTotal, velocityClose }
+      = burndown.burndown(day(2017, 10, 30), day(2017, 11, 2));
+
+  t.deepEqual(downs, [2, 2, 3, 4]);
+  t.deepEqual(ups, [0, 0, 0, 0]);
+  t.is(velocityTotal, 2 / 3);
+  t.is(velocityClose, 0);
+});
+
+test('All closed', t => {
+  let issues = [
+    Issue.closed(day(2017, 10, 29), day(2017, 10, 31)),
+    Issue.closed(day(2017, 10, 30), day(2017, 11, 2)),
+    Issue.closed(day(2017, 11,  1), day(2017, 11, 6)),
+    Issue.closed(day(2017, 11,  2), day(2017, 11, 3)),
+  ];
+  let burndown = new Burndown(issues);
+  let { downs, ups, velocityTotal, velocityClose }
+      = burndown.burndown(day(2017, 10, 30), day(2017, 11, 6));
+
+  t.deepEqual(downs, [2, 1, 2, 2, 1, 0]);
+  t.deepEqual(ups, [0, 1, 1, 2, 3, 4]);
+  t.is(velocityTotal, - 2 / 5);
+  t.is(velocityClose, - 4 / 5);
+});
