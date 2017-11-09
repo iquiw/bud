d7c374d3c74497202574002059ec9d59a4b3352c
Iku Iwasa <iku.iwasa@gmail.com>
2017-11-09 20:24:58 +0900

    Test skipWeekend = false case

--- test/burndown.js
+++ test/burndown.js
@@ -57,3 +57,20 @@ test('All closed', t => {
   t.is(velocityTotal, - 2 / 5);
   t.is(velocityClose, - 4 / 5);
 });
+
+test('Disable skipWeekend option', t => {
+  let issues = [
+    Issue.closed(day(2017, 10, 29), day(2017, 10, 31)),
+    Issue.closed(day(2017, 10, 30), day(2017, 11, 6)),
+    Issue.opened(day(2017, 11,  1)),
+    Issue.opened(day(2017, 11,  2)),
+  ];
+  let burndown = new Burndown(issues, { skipWeekend: false });
+  let { downs, ups, velocityTotal, velocityClose }
+      = burndown.burndown(day(2017, 10, 30), day(2017, 11, 10));
+
+  t.deepEqual(downs, [2, 1, 2, 3, 3, 3, 3, 2, 2, 2, 2, 2]);
+  t.deepEqual(ups, [0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2]);
+  t.is(velocityTotal, 0);
+  t.is(velocityClose, - 2 / 11);
+});
