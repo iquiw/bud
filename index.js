bc73d2eb1eca074ca3c143017b24e707cb990896
Iku Iwasa <iku.iwasa@gmail.com>
2017-11-13 08:42:31 +0900

    Separate issues data from index.js

--- index.js
+++ index.js
@@ -2,23 +2,9 @@ import moment from 'moment';
 
 import { Burndown, Issue } from './lib/burndown';
 import DayChart from './lib/daychart';
-
-function day(y, m, d) {
-  return moment([y, m - 1, d]);
-}
-
-let issues = [
-  Issue.opened(day(2017, 8, 10)),
-  Issue.closed(day(2017, 8, 11), day(2017, 8, 11)),
-  Issue.closed(day(2017, 8, 11), day(2017, 8, 14)),
-  Issue.closed(day(2017, 8, 11), day(2017, 8, 16)),
-  Issue.opened(day(2017, 8, 12)),
-  Issue.closed(day(2017, 8, 12), day(2017, 8, 15)),
-  Issue.closed(day(2017, 8, 14), day(2017, 8, 14)),
-  Issue.closed(day(2017, 8, 15), day(2017, 8, 15))
-];
+import { issues, firstDate, lastDate } from './issues';
 
 let burndown = new Burndown(issues);
 
 let dc = new DayChart(document.getElementById('myChart').getContext('2d'));
-dc.draw(day(2017, 8, 12), day(2017, 8, 18), burndown);
+dc.draw(firstDate, lastDate, burndown);
