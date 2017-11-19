7695563ab71fb02fcb3ec6d4474d56b8271b9cb6
Iku Iwasa <iku.iwasa@gmail.com>
2017-11-20 08:53:08 +0900

    Test with skippedDays and without skipWeekend

--- test/burndown.js
+++ test/burndown.js
@@ -8,6 +8,14 @@ function day(y, m, d) {
   return new Day(y, m, d);
 }
 
+/*
+ *       10月 2017
+ * 日 月 火 水 木 金 土
+ * 29 30 31
+ *       11月 2017
+ *           1  2  3  4
+ *  5  6  7  8  9 10 11
+ */
 test('Burndown', t => {
   let issues = [
     Issue.closed(day(2017, 10, 29), day(2017, 10, 31)),
@@ -94,4 +102,24 @@ test('Specify skippedDays option', t => {
   t.deepEqual(ups, [0, 1, 1, 1, 2, 2, 2, 2]);
   t.is(velocityTotal, 0);
   t.is(velocityClose, - 2 / 7);
+});
+
+test('Specify skippedDays option with skipWeekend disabled', t => {
+  let issues = [
+    Issue.closed(day(2017, 10, 29), day(2017, 10, 31)),
+    Issue.closed(day(2017, 10, 30), day(2017, 11, 6)),
+    Issue.opened(day(2017, 11,  1)),
+    Issue.opened(day(2017, 11,  2)),
+  ];
+  let burndown = new Burndown(issues, {
+    skipWeekend: false,
+    skippedDays: [day(2017, 10, 31), day(2017, 11, 3), day(2017, 11, 6)]
+  });
+  let { downs, ups, velocityTotal, velocityClose }
+      = burndown.burndown(day(2017, 10, 30), day(2017, 11, 10));
+
+  t.deepEqual(downs, [2, 2, 3, 3, 3, 2, 2, 2, 2]);
+  t.deepEqual(ups, [0, 1, 1, 1, 1, 2, 2, 2, 2]);
+  t.is(velocityTotal, 0);
+  t.is(velocityClose, - 2 / 8);
 });
