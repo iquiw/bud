98953557cd7c0a4e25eb1dfe203558e6db938052
Iku Iwasa <iku.iwasa@gmail.com>
2019-09-03 08:54:05 +0900

    Add lastDay argument and use endDay as end of x-axis

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import moment from 'moment';
 
 import { Burndown } from './lib/burndown';
 import DayChart from './lib/daychart';
-import { issues, firstDate, lastDate } from './issues';
+import Day from './lib/day';
+import { issues, firstDate, lastDate, endDate } from './issues';
 
 let burndown = new Burndown(issues);
 
 let dc = new DayChart(document.getElementById('myChart').getContext('2d'));
-dc.draw(firstDate, lastDate, burndown);
+dc.draw(firstDate, lastDate, endDate, burndown);
