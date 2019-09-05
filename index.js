cc89235778cd5b96e23f72d7a7d9ab3b6e0f0bac
Iku Iwasa <iku.iwasa@gmail.com>
2019-09-05 21:28:02 +0900

    Rename endDay to dueDay, lastDay to endDay

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import moment from 'moment';
 import { Burndown } from './lib/burndown';
 import DayChart from './lib/daychart';
 import Day from './lib/day';
-import { issues, firstDate, lastDate, endDate } from './issues';
+import { issues, startDay, endDay, dueDay } from './issues';
 
 let burndown = new Burndown(issues);
 
 let dc = new DayChart(document.getElementById('myChart').getContext('2d'));
-dc.draw(firstDate, lastDate, endDate, burndown);
+dc.draw(startDay, endDay, dueDay, burndown);
