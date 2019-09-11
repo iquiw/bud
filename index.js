cf642274714f0d5b7456201c147ef39685ee14ba
Iku Iwasa <iku.iwasa@gmail.com>
2019-09-12 07:11:15 +0900

    Make drawDayChart() take options for burndown and daychart

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ import { Burndown, Issue } from './lib/burndown';
 import DayChart from './lib/daychart';
 import Day from './lib/day';
 
-export function drawDayChart(vis) {
+export function drawDayChart(vis, burndownOptions, dayChartOptions) {
   let burndown = new Burndown(
     vis.issues.map((issue) => new Issue(Day.fromString(issue.open),
-                                        issue.close ? Day.fromString(issue.close) : null)));
+                                        issue.close ? Day.fromString(issue.close) : null)),
+    burndownOptions);
 
-  let dc = new DayChart(document.getElementById('myChart').getContext('2d'), {
+  let dc = new DayChart(document.getElementById('myChart').getContext('2d'), Object.assign({
     title: {
       display: true,
       position: 'bottom',
       text: vis.version,
-    },
-    showEstimate: true
-  });
+    }
+  }, dayChartOptions));
   dc.draw(
     Day.fromString(vis.startDate),
     Day.fromString(vis.endDate),
